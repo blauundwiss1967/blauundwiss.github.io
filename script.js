be015f70d2582fd2e9bd2a05eec784856faccf16
blauundwiss1967 <37964367+blauundwiss1967@users.noreply.github.com>
2018-03-31 23:35:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+/*function myFunction() {
+  var Zahl=9999/3.145159;
+  var Pe=Zahl.toFixed(1);
+    document.getElementById("demo").innerHTML =Pe;}*/
+
+
+
+/*var zahl=prompt("Gib eine Zahl ein");
+
+   if (zahl < 0 || zahl >10 ) {
+
+      alert("Gib eine andere Zahl ein!");}
+
+
+    else if (zahl < 10 || zahl > 0) {
+
+      alert("Deine Zahl ist "+ zahl)}*/
+
+
+
+var d = new Date();
+var a = d.getDate()
+var m = new Date();
+var o = m.getMonth() + 1;
+var y = new Date();
+var e = y.getFullYear();
+
+document.write('<p>' +"7.2.2018"+" ~ "+ a +"." + o+"." + e + '</p>');
